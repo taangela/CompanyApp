5d9a92372d0bd8b742965eed735e293bf44901c7
Marta Koperek <makoperek@gmail.com>
2020-02-26 23:35:59 +0100

    change endpoints in departments

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
-const app = express(;)
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api', employeesRoutes);
+//app.use('/api', employeesRoutes);
 app.use('/api', departmentsRoutes);
-app.use('/api', productsRoutes);
+//app.use('/api', productsRoutes);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
