465f79bcec00c34985a9d96969fe34c0add82a37
Marta Koperek <makoperek@gmail.com>
2020-02-27 10:57:50 +0100

    introduces references between mongo collection

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -5,7 +5,7 @@ const Employees = require('../models/Employees.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employees.find());
+    res.json(await Employees.find().populate('department'));
   }
   catch(err) {
     res.status(500).json(err)
@@ -17,7 +17,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employees.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const dep = await Employees.findOne().skip(rand);
+    const dep = await Employees.findOne().populate('department').skip(rand);
     if(!dep) res.status(404).json({ message: 'Not found' });
     else res.json(dep);
   }
@@ -30,7 +30,7 @@ router.get('/employees/random', async (req, res) => {
 router.get('/employees/:id', async (req, res) => {
 
   try {
-    const dep = await Employees.findById(req.params.id);
+    const dep = await Employees.findById(req.params.id).populate('department');
     if(!dep) res.status(404).json({ message: 'Not found' });
     else res.json(dep);
   }
@@ -62,7 +62,7 @@ router.put('/employees/:id', async (req, res) => {
     const dep = await(Employees.findById(req.params.id));
     if(dep) {
       await Employees.updateOne({ _id: req.params.id }, { $set: { firstName: firstName, lastName: lastName, department: department }});
-      res.json({ message: 'OK' });
+      res.json(await Employees.find().populate('department'));
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -78,7 +78,7 @@ router.delete('/employees/:id', async (req, res) => {
     const dep = await(Employees.findById(req.params.id));
     if(dep) {
       await Employees.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json(await Employees.find().populate('department'));
     }
     else res.status(404).json({ message: 'Not found...' });
   }
